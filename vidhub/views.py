c024ac135e0d86a8b7c250911bd63020f7179e7a
Krittamate Tiankanon <boomzaza@mit.edu>
2019-09-14 16:17:44 -0400

    Add mp3 converter

--- vidhub/views.py
+++ vidhub/views.py
@@ -2,13 +2,13 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 from django import forms
-
+from django.conf import settings
 from .models import Video
-
-
+import moviepy.editor as mp
 # Create your views here.
 def index(request):
     return HttpResponse("Hello, world.")
+
 
 
 '''
@@ -23,11 +23,18 @@ def upload(request):
         uploadedFile = request.FILES['file']
         fileName = "storage/"+uploadedFile.name
         Video.objects.create(title=request.POST['title'], author=request.POST['author'], video_url=uploadedFile.name, rev_id=0, subtitle_url='')
-
+        # Write File mp4
         with open(fileName, "wb") as f:
             for chunk in uploadedFile.chunks():
                 f.write(chunk)
-        return HttpResponse("Uploaded")
+
+        # Convert to mp3
+        
+        clip = mp.VideoFileClip(fileName).subclip(0)
+        clip.audio.write_audiofile(fileName[:-4]+".mp3")
+        #
+        
+        return HttpResponse("Uploaded" + settings.REV_ACCESS_TOKEN)
     else:
         return HttpResponse("WTF")
 
