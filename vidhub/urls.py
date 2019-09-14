c2d2896f6bde796afcd56c274e1d1186351b213d
Pasapol Saowakon <zoomswk@mit.edu>
2019-09-14 15:52:03 -0400

    added rev.ai callback url

--- vidhub/urls.py
+++ vidhub/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('upload/', views.upload, name='upload'),
     path('vid/<int:uid>', views.vid, name='vid'),
+    path('apis/revai/', views.revai_callback, name='revai_callback'),
 ]
