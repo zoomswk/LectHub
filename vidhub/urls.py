4541841ed604cdb697aa4425370792619efa2f2e
Krittamate Tiankanon <boomzaza@mit.edu>
2019-09-14 13:55:57 -0400

    Add upload.py nad upload url

--- vidhub/urls.py
+++ vidhub/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
+from . import upload
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('upload/',upload.index, name='upload')
 ]
