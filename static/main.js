89759573026d9ea57330f32f083a4fab1c042836
Krittamate Tiankanon <boomzaza@mit.edu>
2019-09-14 23:00:34 -0400

    Try another UPDATE URL

--- static/main.js
+++ static/main.js
@@ -26,7 +26,7 @@ function update_subtitle(s, render) {
               console.log("s = " + s);
 
               // send a new request
-              $.post("/update/", {block_id: cur_id, new_dialog: s});
+              $.post("vidtube/vid/"+$('#id').html()+"/update/", {block_id: cur_id, new_dialog: s});
 
               display(1, true);
 
